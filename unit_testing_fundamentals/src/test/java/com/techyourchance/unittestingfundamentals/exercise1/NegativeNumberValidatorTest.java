6dccf123e71b6c89ba1549132a0b697bf3c9cc29
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-11-29 15:02:58 -0200

    Exercise 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,39 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import com.techyourchance.unittestingfundamentals.example1.PositiveNumberValidator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
 
+    public NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void SHOULD_ReturnTrue_WHEN_NumberIsNegative() {
+        boolean result = SUT.isNegative(-1);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void SHOULD_ReturnFalse_WHEN_NumberIsPositive() {
+        boolean result = SUT.isNegative(1);
+        Assert.assertThat(result, is(false));
+
+    }
+
+    @Test
+    public void SHOULD_ReturnFalse_WHEN_NumberIsZero() {
+        boolean result = SUT.isNegative(0);
+        Assert.assertThat(result, is(false));
+
+    }
 }
