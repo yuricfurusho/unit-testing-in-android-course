7c19f96989b41243cc0a717d4052f8493695fe9a
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-11-29 15:46:45 -0200

    Exercise 2 Completed!

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
+
+    StringDuplicator mSUT;
+
+    @Before
+    public void setUp() throws Exception {
+        mSUT = new StringDuplicator();
+    }
+
+    @Test
+    public void SHOULD_returnEmpty_WHEN_StringIsEmpty() {
+        String result = mSUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void SHOULD_returnDoubleCharacter_WHEN_StringIsSingleCharacter() {
+        String result = mSUT.duplicate("a");
+        assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void SHOULD_returnDuplicatedLongString_WHEN_StringIsLong() {
+        String result = mSUT.duplicate("Yuri Furusho");
+        assertThat(result, is("Yuri FurushoYuri Furusho"));
+    }
 
 }
